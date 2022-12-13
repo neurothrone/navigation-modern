9307a9dfab2deecceeb89fb75022252a3ff9bb88
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-13 01:54:13 +0100

    set up project

---
+++ NavigationOptimal/AppMain.swift
@@ -0,0 +1,17 @@
+//
+//  AppMain.swift
+//  NavigationOptimal
+//
+//  Created by Zaid Neurothrone on 2022-12-13.
+//
+
+import SwiftUI
+
+@main
+struct AppMain: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
